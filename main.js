6646160de5d4a1994868f8628f505a4e883d5c38
Whale1902 <lubimov1902@gmail.com>
2023-02-09 17:22:20 +0200

    refactor: move static widgets logic back to main.js file

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ import {
   removeSpinner,
 } from "./scripts/helpers.js";
 
-import { addTodoLogic, addSearchLogic } from "./scripts/staticWidgetsLogic.js";
 import {
   renderTodosFromLocalStorage,
   saveTodosToLocalStorage,
@@ -92,6 +91,73 @@ const fetchWeather = function () {
   renderSpinner(dom.weather.widget);
   getData(DYNAMIC_URLS(profileSettings, "weather"), renderWeather);
 };
+
+////////////////////////////////////// TODO WIDGET  //////////////////////////////////////
+
+dom.todo.list.addEventListener("click", function (e) {
+  const clicked = e.target.classList.value;
+
+  if (clicked === "todo__list") {
+    return;
+  } else if (clicked.includes("delete")) {
+    e.target.closest(".todo__item").remove();
+  } else if (clicked.includes("todo__item")) {
+    e.target.classList.toggle("todo__item--crossed");
+  } else if (!clicked.includes("todo__item")) {
+    e.target.closest(".todo__item").classList.toggle("todo__item--crossed");
+  }
+});
+
+dom.todo.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const todoMarkup = `
+    <div class="todo__item">
+      <p class="todo__text">${dom.todo.input.value}</p>
+      <button class="todo__delete">&#9587;</button>
+    </div>
+  `;
+
+  if (!dom.todo.input.value) return;
+
+  dom.todo.list.insertAdjacentHTML("beforeend", todoMarkup);
+
+  dom.todo.input.value = "";
+  dom.todo.input.focus();
+});
+
+dom.todo.clearButton.addEventListener("click", function () {
+  for (let el of document.querySelectorAll(".todo__item")) {
+    el.remove();
+  }
+});
+
+////////////////////////////////////// SEARCH WIDGET  //////////////////////////////////////
+
+// Allow user to search with at least one character in query
+dom.search.input.addEventListener("input", function () {
+  // Global selector has been set on page loading, so I need to update it
+  dom.search.input = document.getElementById("search__form__input");
+
+  if (dom.search.input.value.length === 0) {
+    dom.search.submit.disabled = true;
+  } else if (dom.search.input.value.length > 0) {
+    dom.search.submit.disabled = false;
+  }
+});
+
+dom.search.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Selecting only checked option
+  const searchCheckedOption = document.querySelector(
+    'input[name="search__option"]:checked'
+  );
+
+  const targetUrl = `${searchCheckedOption.getAttribute("data-url")}${
+    dom.search.input.value
+  }`;
+  window.open(targetUrl, "_blank");
+});
 
 ////////////////////////////////////// CRYPTO WIDGET  //////////////////////////////////////
 const renderCrypto = function (data) {
@@ -186,8 +252,6 @@ const init = function () {
   fetchWeather();
   fetchCrypto();
   fetchNews();
-  addSearchLogic();
-  addTodoLogic();
 };
 
 // init();
